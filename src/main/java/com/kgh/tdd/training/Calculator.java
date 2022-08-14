fc48546743cc93e57d28183cb472251c2c77cf88
kgh <gwanhyeon0525@gmail.com>
2022-08-14 16:00:54 +0900

    feat: tdd plus validation

---
+++ src/main/java/com/kgh/tdd/training/Calculator.java
@@ -0,0 +1,7 @@
+package com.kgh.tdd.training;
+
+public class Calculator {
+    static int plus(int a1, int a2){
+        return a1+a2;
+    }
+}
--- src/test/java/com/kgh/tdd/training/TddTrainingApplicationTests.java
+++ src/test/java/com/kgh/tdd/training/TddTrainingApplicationTests.java
@@ -3,6 +3,8 @@ package com.kgh.tdd.training;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class TddTrainingApplicationTests {
 
@@ -10,4 +12,13 @@ class TddTrainingApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void plus(){
+		int result = Calculator.plus(1,2);
+		assertEquals(3, result);
+		assertEquals(5, Calculator.plus(4,1));
+
+	}
 }
+
+
